eaa28870ee9e83bc116196f668f9782ae7b25354
Ilya Buziuk <ibuziuk@exadel.com>
2013-06-28 18:53:23 +0300

    Adding test for Globalization API

--- www/test/phonegap.js
+++ www/test/phonegap.js
@@ -1,79 +1,81 @@
-describe("Phonegap", function() {
+describe("Phonegap", function () {
 
-	it("Device API", function() {
+    it("Device API", function () {
         console.log("Testing Notification API");
 
         expect(device).toBeDefined();
-		expect(device).not.toBe(null);
+        expect(device).not.toBe(null);
 
         expect(device.cordova).toBeDefined();
-		expect(device.cordova).not.toBe(null);
+        expect(device.cordova).not.toBe(null);
 
         expect(device.platform).toBeDefined();
-		expect(device.platform).not.toBe(null);
+        expect(device.platform).not.toBe(null);
 
         expect(device.uuid).toBeDefined();
-		expect(device.uuid).not.toBe(null);
+        expect(device.uuid).not.toBe(null);
 
         expect(device.version).toBeDefined();
         expect(device.version).not.toBe(null);
-	 });
+    });
 
-	it("Notification API", function() {
+    it("Notification API", function () {
         // TODO This test will not pass only when cordova.js isn't found
         console.log("Testing Notification API");
 
-		expect(navigator.notification).toBeDefined();
+        expect(navigator.notification).toBeDefined();
         expect(navigator.notification.alert).toBeDefined();
-	});
+    });
 
-	it("Compass API", function() {
-		console.log("Testing Compass API");
+    it("Compass API", function () {
+        console.log("Testing Compass API");
 
         var obj = {
-            success:function(heading) {
+            success: function (heading) {
                 expect(heading.magneticHeading).toBeDefined();
                 expect(heading.magneticHeading).not.toBeNull();
             },
-            error:function() {
+            error: function () {
                 console.log("Error while simulating Compass API");
             }
         };
         spyOn(obj, "success").andCallThrough();
         spyOn(obj, "error").andCallThrough();
 
-        runs(function() {
+        runs(function () {
             navigator.compass.watchHeading(obj.success, obj.error);
         });
 
-        waitsFor(function(){
+        waitsFor(function () {
             return obj.success.callCount > 0 || obj.error.callCount > 0;
         });
 
-        runs(function() {
+        runs(function () {
             expect(obj.success.callCount).toBe(1);
             expect(obj.error.callCount).toBe(0);
         });
-	});
+    });
 
-    it("Storage API", function() {
+    it("Storage API", function () {
         console.log("Testing Storage API");
 
         var obj = {
-            success:function (tx, results) {
+            success: function (tx, results) {
                 var len = results.rows.length, i;
                 for (i = 0; i < len; i++) {
                     obj.valueChecker(results.rows.item(i).text);
                 }
             },
-            error:function(){},
-            valueChecker:function(){}
+            error: function () {
+            },
+            valueChecker: function () {
+            }
         };
         spyOn(obj, 'success').andCallThrough();
         spyOn(obj, 'error');
         spyOn(obj, 'valueChecker');
-        runs(function(){
-            var db = openDatabase('mydb', '1.0','mydb', 2 * 1024 * 1024);
+        runs(function () {
+            var db = openDatabase('mydb', '1.0', 'mydb', 2 * 1024 * 1024);
             db.transaction(function (tx) {
                 tx.executeSql('DROP TABLE IF EXISTS foo');
                 tx.executeSql('CREATE TABLE IF NOT EXISTS foo (id unique, text)');
@@ -81,21 +83,21 @@ describe("Phonegap", function() {
                 tx.executeSql('SELECT * FROM foo', [], obj.success, obj.error);
             });
         });
-        waitsFor(function(){
+        waitsFor(function () {
             return obj.success.callCount > 0 || obj.error.callCount > 0
         });
-        runs(function(){
+        runs(function () {
             expect(obj.success.callCount).toBe(1);
             expect(obj.error.callCount).toBe(0);
             expect(obj.valueChecker).toHaveBeenCalledWith('synergies');
         });
     });
 
-    it("Geolocation API", function() {
+    it("Geolocation API", function () {
         console.log("Testing Geolocation API");
 
         var obj = {
-            success:function(position) {
+            success: function (position) {
                 expect(position.coords).toBeDefined();
 
                 expect(position.coords.longitude).toBeDefined();
@@ -116,41 +118,115 @@ describe("Phonegap", function() {
                 expect(position.timestamp).toBeDefined();
                 expect(position.timestamp).not.toBeNull();
             },
-            error:function() {
+            error: function () {
                 console.log("Error while simulating Geolocation API");
             }
         };
         spyOn(obj, "success").andCallThrough();
         spyOn(obj, "error").andCallThrough();
 
-        runs(function() {
+        runs(function () {
             navigator.geolocation.getCurrentPosition(obj.success, obj.error);
         });
 
-        waitsFor(function(){
+        waitsFor(function () {
             return obj.success.callCount > 0 || obj.error.callCount > 0;
         });
 
-        runs(function() {
+        runs(function () {
             expect(obj.success.callCount).toBe(1);
             expect(obj.error.callCount).toBe(0);
         });
     });
 
-    it("Connection API", function() {
+    it("Connection API", function () {
         console.log("Testing Geolocation API");
 
         var networkState = navigator.connection.type;
         var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.UNKNOWN] = 'Unknown connection';
         states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.CELL]     = 'Cell generic connection';
-        states[Connection.NONE]     = 'No network connection';
+        states[Connection.WIFI] = 'WiFi connection';
+        states[Connection.CELL_2G] = 'Cell 2G connection';
+        states[Connection.CELL_3G] = 'Cell 3G connection';
+        states[Connection.CELL_4G] = 'Cell 4G connection';
+        states[Connection.CELL] = 'Cell generic connection';
+        states[Connection.NONE] = 'No network connection';
 
-        expect(states[networkState]).toBe( states[Connection.ETHERNET]); // By Default "Ethernet" Connection is used in the CordovaSim (it can be changed in the "Device & Network Setting" tab)
+        expect(states[networkState]).toBe(states[Connection.ETHERNET]); // By Default "Ethernet" Connection is used in the CordovaSim (it can be changed in the "Device & Network Setting" tab)
+    });
+
+    it("Globalization API", function () {
+        console.log("Testing Globalization API");
+
+        expect(navigator.globalization).toBeDefined();
+        var obj = {
+            successLanguage: function (language) {
+                expect(language).toBeDefined();
+                expect(language.value).toEqual(jasmine.any(String));
+            },
+            errorLanguage: function () {
+                console.log("Globalization preferredLanguage error");
+            },
+            successLocalName: function (locale) {
+                expect(locale).toBeDefined();
+                expect(locale.value).toEqual(jasmine.any(String));
+            },
+            errorLocalName: function () {
+                console.log("Globalization localName error");
+            },
+            successDatePattern: function (date) {
+                expect(date).toBeDefined();
+            },
+            errorDatePattern: function () {
+                console.log("Globalization datePattern error");
+            }
+        };
+        spyOn(obj, 'successLanguage').andCallThrough();
+        spyOn(obj, 'errorLanguage').andCallThrough();
+
+        spyOn(obj, 'successLocalName').andCallThrough();
+        spyOn(obj, 'errorLocalName').andCallThrough();
+
+        spyOn(obj, 'successDatePattern').andCallThrough();
+        spyOn(obj, 'errorDatePattern').andCallThrough();
+
+        runs(function () {
+            console.log("Testing Globalization getPreferredLanguage method");
+            navigator.globalization.getPreferredLanguage(obj.successLanguage, obj.errorLanguage);
+        });
+
+        waitsFor(function () {
+            return obj.successLanguage.callCount > 0 || obj.errorLanguage.callCount > 0;
+        });
+
+        runs(function () {
+            console.log("Testing Globalization getPreferredLanguage method");
+            navigator.globalization.getLocaleName(obj.successLocalName, obj.errorLocalName);
+        });
+
+        waitsFor(function () {
+            return obj.successLocalName.callCount > 0 || obj.errorLocalName.callCount > 0;
+        });
+
+        runs(function () {
+            console.log("Testing Globalization getDatePattern method");
+            navigator.globalization.getDatePattern(obj.successDatePattern, obj.errorDatePattern);
+        });
+
+        waitsFor(function () {
+            return obj.successDatePattern.callCount > 0 || obj.errorDatePattern.callCount > 0;
+        });
+
+        runs(function () {
+            expect(obj.successLanguage.callCount).toBe(1);
+            expect(obj.errorLanguage.callCount).toBe(0);
+
+            expect(obj.successLocalName.callCount).toBe(1);
+            expect(obj.errorLocalName.callCount).toBe(0);
+
+            expect(obj.successDatePattern.callCount).toBe(1);
+            expect(obj.errorDatePattern.callCount).toBe(0);
+        });
     });
 });
